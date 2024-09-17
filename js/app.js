7a66831920a90d8eae68110d3e29ee1cf1a4e573
Yevhen K. <zhekan89@gmail.com>
2024-09-17 22:03:13 +0300

    save

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 (() => {
     "use strict";
     const modules_flsModules = {};
-    function addLoadedClass() {
-        if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
-            setTimeout((function() {
-                document.documentElement.classList.add("loaded");
-            }), 0);
-        }));
-    }
     function getHash() {
         if (location.hash) return location.hash.replace("#", "");
     }
@@ -4167,7 +4160,6 @@
     const da = new DynamicAdapt("max");
     da.init();
     window["FLS"] = false;
-    addLoadedClass();
     menuInit();
     spollers();
     formFieldsInit({
